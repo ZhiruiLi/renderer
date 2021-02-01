5556d5f78dac3f8006d3d35c0410b4495a0bcde4
zhiruili <zr.public@outlook.com>
2021-02-01 16:47:25 +0800

    update main
    Former-commit-id: 0ee755d2a71426593b9d5975e306f9ec4b3d0f7f [formerly 3a7ca6eeae780d69a7aa95dfc6f9016df3af831d] [formerly ca9d844a10dcdbe696ec70bbfaed27cb456350b8 [formerly f363c396477912557a97758bdd03df9ee6c1d5a0]] [formerly 789c94593c63499bf41ce67e12407a190d943d5e [formerly 42c954c7e850fb44eb65653ccb5e9122f3a8a6f4] [formerly d9879b7517dd79e1e98667e6f7db85c966e063c4 [formerly 6f2d3e96c3cd62a002107cd09a185759759cbc34]]] [formerly 73895963b1f563b5d369a47851dcc16301b3d9ac [formerly abf7c3b89fe66004b73ec0eb49ee3b0dab500f54] [formerly 8c33c6ef4fb76a3b785aa4d5be5fe2bb55132a35 [formerly ba2987e5fec6a290240557e59f576b2cf240b2f1]] [formerly 347cc12910ae0ea1ad82a51c2bf0f82c521551c2 [formerly a37487f7f50f803f80a14cbef815202c4c8fad28] [formerly c9ba0fe78e495d2e3463a50d17f6ca5ae4976dff [formerly 250ff64a1d0487f5158a2eab5e336dc9426f7a55]]]] [formerly 289812ceec8202e433434f2220a6e22f4274cdfa [formerly c67b5570bf2a5d0a6c45ff182a9ed687f3e1db6f] [formerly 89f5299642d59294ce0a70b7270c6f8aae7dd19a [formerly 17c15bde425f1dfc67dd2839c015e9a6717bcf0c]] [formerly 9d7dd4405eaf90c64b7d6482d0d7dac4a78665cf [formerly 1f0f252fa4da3a06e87e69eb1c1bd58f5f804a34] [formerly 079185fb4f4b1633560ced9a2067968ad188404d [formerly 0202915509edd59ca52190caaefc150403de81b1]]] [formerly 69a3371c95c03d99f2eb8998fba1ff288747b81d [formerly c4eb9ee2f57757ae82535f7cd9de45e51d72100a] [formerly 24bab5ae8122612785d5046aa7c6f1bf14cfe533 [formerly 087460fdd96fa60e0ac7ad0e1623cd35d71fe2a8]] [formerly f4defdbeff47e4ff987a9f5f9185a59982538f6c [formerly b2ce207a13226b0ca751e38fdbfbac4c1ebd7d1f] [formerly 65a6de88987f1f73fc9999b773d1b70d7a8cc4ca [formerly 95e210dbd2948ffa839386cc79ac4b197f3ea2cb]]]]]
    Former-commit-id: 13057b9df448120f558deb20df19f048f71c5aed [formerly 58c70628a4ed4feb6323d7d7eb63eae40b994f79] [formerly 847b0550bc06dc3d61ef80ac4a758d79bac03cb9 [formerly 96926fcbe74c13daeeb1d9c585138d45c655060b]] [formerly d740d8a8d152f461c157c0a507a3c465dd13089a [formerly b1be676c3230c7480aee1458abe0761b0f9d9986] [formerly f0c28607574743423c52cadef78564c038dcb034 [formerly 1027c5cd87e6b7ef6af9723a8bcaafe268489fe1]]] [formerly e3c17d312115ee19c9128f9e7bcb779144d6798e [formerly e8c5cec686fc31d2d8fa006d192b48289b770dca] [formerly 86c1ec9c3624b6b37ccd9127a29b1b61d671bb97 [formerly c89486fcc86c19b31a7aa24e61a6b9a80c2a5d1e]] [formerly e75043dc9248e399cdb441354b422c1ecf02b06d [formerly 18104ba63ac58ffada627f6706d5a01ddfb11970] [formerly 982cb45980b6701c47cf981aa28643152dfab6be [formerly 032fd218a30f8b3231893dd8a26febf4e7391abe]]]]
    Former-commit-id: f423926142326f41d4b6eb08c8a12a7c1d3ad4e1 [formerly 8081a7925241609109d1ace3fc17bfa361e5a02a] [formerly 8acef60bcfdee0c90539cd261a916cfb4b8dd833 [formerly eff74d6d5247711822fa455302b9c14ef2df5014]] [formerly c6beaa9692abfe9107a240ac407859aa7ab1dabc [formerly 3421acb823911cda3b2830c66726329971c7fb82] [formerly 459beef5bdc2885048d3e621178fdaf628c0b050 [formerly b65ab383fa8b8c305b3f380814eeaca8e34e71b5]]]
    Former-commit-id: f10e0fdf8b9a4501e9cb12994b14f18d284d47a3 [formerly 7f0c68b49056b027478f38a919b9c65025b79bcc] [formerly cb5caa577e3bc7ef995f7fc0707b19afc577ddae [formerly 6f509373e0027986e1e41aab24382cb7701d0ef1]]
    Former-commit-id: 179185d99e12b759391093b10b0c637462f28423 [formerly 7d87217b7c35e25127503ad59e883ff27c9a45b0]
    Former-commit-id: 1abf78d339b15cbe9b5cca56403001a1bb4cd9f1

--- src/main.cc
+++ src/main.cc
@@ -9,6 +9,7 @@ int main() {
   window.set_main_loop([](sren::FrameBuffer *fb) {
     fb->Clear();
     sren::DrawTriangle({50.0f, 60.0f}, {30.0f, 40.0f}, {30.0f, 50.0f}, red, fb);
+    sren::DrawTriangle({299.0f, 199.0f}, {99.0f, 99.0f}, {0.0f, 0.0f}, red, fb);
     sren::DrawLine({0.0f, 0.0f}, {99.0f, 99.0f}, red, fb);
     sren::DrawLine({299.0f, 199.0f}, {0.0f, 0.0f}, red, fb);
   });
