d00848ef0366890560e74d291f86c5a7ede0e690
zhiruili <zr.public@outlook.com>
2021-02-11 14:27:49 +0800

    update camera
    Former-commit-id: 5e883a58174f807c51d1f6a13891597478d263f2 [formerly 92514cdd53af2ee8a64b9377c81ab65a8ed59b46] [formerly 8ad0001c89b93d9a1f9cdeecade80f3f991504f6 [formerly cee1fb7d31473cb0ade3c862448e56138da824f1]] [formerly 1e6c7d8d609ca7cbcc6ad0e2b4a7e347378702d3 [formerly 2b207cfd2ea9a9599c2434f5cb08d8da432063b9] [formerly 01fde552b838e04f1c26828db565ba6674f33922 [formerly 6dd4e5621f80ba22248253472419a21f4f09e286]]] [formerly a75961b7a23872a4dd7f2964d9b0f319f643ff85 [formerly b0505dd76bbb5267c2d7feea950466cc8e40232a] [formerly 8d22b7f7e10b95c1060cbccedfdbb3d01dbfd137 [formerly 0065262821fa658c99a8e6cedae518cafb8a3f24]] [formerly e99f36cfa16c918fc108fdc4352c15bb14e8b4c5 [formerly 5e8d5204c4f76c22f9bacf76a62899f5651912b0] [formerly 3d533be69e5bad1ae6e29de568762030e304e756 [formerly cddaaa9a362c92aad8cbbd29af2a0f8d902a3959]]]] [formerly d84de9ece90c1fff5d04c7895148e1a1ae5dd68c [formerly 0841e5dd65f4d635bd48c39f75e975743d7a1834] [formerly 6d11e8d461d1c11bb94374b8a6a4bed4fc3c3992 [formerly dc69f107517dc81c584406d5ac675a6552d0a552]] [formerly ea6479d6056e663c761e34c553d2d0401dbee7f1 [formerly e06c2668f1858ca8cdc130f11a918d77ab8ef0ff] [formerly 5c787db73fefc8a34a1f8a86097e672d387a3552 [formerly 4c649ee77b7edc9fd9440ac4792d5ff4cd97b28f]]] [formerly f96d6f2d62e82c424e0bbf21d0ab29fc9f46203e [formerly 8cdb841ca8b382370de1ec843ac21dd2d0e80301] [formerly b0ffa6405e7bfa00d117153f3dff11536898c799 [formerly 823577808e50afa6c75c2e0eafdfd580a8f3b249]] [formerly 87cc47ece9acae3dad0ee56bf0493d0245d28581 [formerly aeadc7945ce1bed8dfe63d34c1f8547702389b14] [formerly d985f107044c7b8e6a2be846cea618299c72ebd3 [formerly 3029ff169821f90a513dd22b2fc0e1b3d35ee851]]]]]
    Former-commit-id: 985e4377676ecfded1b315c81216d3c6fc85f3ce [formerly 68838fb70a2104460bdf1276a12fa752565796c6] [formerly 181a58e2dc7dc2effcb2929a40f4b598e9e8225a [formerly fccea4dad62bd39fcdce851486e553a6be706069]] [formerly c0e892aa136f6a41fa1560cab902c9f773edb9b8 [formerly b3a3fe75810cfe01df411d43fd825835685e81c3] [formerly 0aa2f9ba717040e68ffa0df4585d1c9826a0295e [formerly de0b1bf399c627ea27d37b647a2a5cd28a57e128]]] [formerly b61fd2d00ce241110a04748462671a651b0810e1 [formerly 14746562b607253fff9a12d7acefa51d3afaa2e4] [formerly bafc12fea48c25f9bfc1aed0b64c95472d88de23 [formerly 9a11a4c86a4b1d2fe47f56f85dbb75bb665da67d]] [formerly b3dabc77a112548eb59ca1e24df8656fd985f0d2 [formerly cdce21decea62ee9955dfa3c20d6d58c2bcafaa8] [formerly 5ad7e20e94280293dea97a5fcba04e7ea6e423a3 [formerly c5499945ca281992e03291973cd008776bf5c541]]]]
    Former-commit-id: 7bdd2bfe75882cc1643e3719dab33b8e852a4132 [formerly f4463c05f9af0b9eb378edd0aeb84416ba5b6992] [formerly b5e6fab329387dc5755b0ec6ac9104ec98aa4e40 [formerly bd100d9a888500fe35274bc41c535a499236f3a0]] [formerly 9238f6b99fd6d469e466e4377084dca099a0b3dd [formerly 22b24c9882f79d07ff5cb879767b246f4272c81f] [formerly c9b3264d5ec52c9c94a78ea689c7f92c19b3cba6 [formerly 4b88addb30a416fd64d666cf7bfe1585a95763fa]]]
    Former-commit-id: 966b2d2694e2c25d57c3d24668795d4bb0774a55 [formerly efd12c301b9e6e39e067e46145340b1b63c98906] [formerly 84764848e827fd303950eba0ec2a523a098a3b0a [formerly f1c8a0a0e90ca49f902871b7a3ae67c941550a17]]
    Former-commit-id: b8e979e8922c485fcb7993afcf0ae403f4890a03 [formerly 456ce838892114031902465fe51facaf6172426e]
    Former-commit-id: be616a178e12269190adc1963fed7205e9c9797a

---
+++ src/camera.cc
@@ -0,0 +1,76 @@
+#include "camera.h"
+
+namespace sren {
+
+Camera& Camera::SetLookAt(Point3 const& pos, Point3 const& target,
+                          Vector3 const& up) {
+  pos_ = pos;
+  target_ = target;
+  up_ = up;
+  view_dirty_ = true;
+  transform_dirty_ = true;
+  return *this;
+}
+
+Camera& Camera::SetPerspective(float fov_radian_v, float aspect,
+                               float near_clip, float far_clip) {
+  fov_radian_v_ = fov_radian_v;
+  aspect_ = aspect;
+  near_clip_ = near_clip;
+  far_clip_ = far_clip;
+  view_dirty_ = true;
+  projection_dirty_ = true;
+  return *this;
+}
+
+Matrix4x4 const& Camera::view_matrix() const {
+  if (view_dirty_) {
+    view_matrix_ = CalcViewMatrix();
+    view_dirty_ = false;
+  }
+  return view_matrix_;
+}
+
+Matrix4x4 const& Camera::projection_matrix() const {
+  if (projection_dirty_) {
+    projection_matrix_ = {};  // TODO: impl projection matrix calc
+    projection_dirty_ = false;
+  }
+  return projection_matrix_;
+}
+
+Matrix4x4 const& Camera::transform_matrix() const {
+  if (transform_dirty_) {
+    transform_matrix_ = view_matrix() * projection_matrix();
+    transform_dirty_ = false;
+  }
+  return transform_matrix_;
+}
+
+Matrix4x4 Camera::CalcViewMatrix() const {
+  Vector3 const n = (target_ - pos_).Normalize();
+  Vector3 const u = (n ^ up_).Normalize();
+  Vector3 const v = u ^ n;
+  auto const posv = pos_ - Point3::Zero();
+  return Matrix4x4({{
+      {u.x(), v.x(), n.x(), 0.0f},
+      {u.y(), v.y(), n.y(), 0.0f},
+      {u.z(), v.z(), n.z(), 0.0f},
+      {-(posv * u), -(posv * v), -(posv * n), 1.0f},
+  }});
+}
+
+Matrix4x4 Camera::CalcProjectionMatrix() const {
+  auto const n = near_clip();
+  auto const f = far_clip();
+  auto const fov_h = fov_radian_v_;  // theta
+  auto const fov_v = fov_h * aspect_;
+  return Matrix4x4({{
+      {1 / tan(fov_h / 2), 0.0f, 0.0f, 0.0f},
+      {0.0f, 1 / tan(fov_v / 2), 0.0f, 0.0f},
+      {0.0f, 0.0f, f / (f - n), 1.0f},
+      {0.0f, 0.0f, f * n / (n - f), 0.0f},
+  }});
+}
+
+}  // namespace sren
--- src/camera.h
+++ src/camera.h
@@ -11,92 +11,36 @@ class Camera {
   Camera() = default;
   ~Camera() {}
 
-  Camera& SetLookAt(Point3 const& pos, Point3 const& target,
-                    Vector3 const& up = {0, 1, 0}) {
-    pos_ = pos;
-    target_ = target;
-    up_ = up;
-    view_dirty_ = true;
-    transform_dirty_ = true;
-    return *this;
+  Camera& SetLookAt(Point3 const& pos, Point3 const& target) {
+    return SetLookAt(pos, target, {0, 1, 0});
   }
 
-  Camera& SetPerspective(float fov_radian, float aspect) {
-    return SetPerspective(fov_radian, aspect, near_clip(), far_clip());
+  Camera& SetLookAt(Point3 const& pos, Point3 const& target, Vector3 const& up);
+
+  Camera& SetPerspective(float fov_radian_v, float aspect) {
+    return SetPerspective(fov_radian_v, aspect, near_clip(), far_clip());
   }
 
-  Camera& SetPerspective(float fov_radian, float aspect, float near_clip,
-                         float far_clip) {
-    fov_radian_ = fov_radian;
-    aspect_ = aspect;
-    near_clip_ = near_clip;
-    far_clip_ = far_clip;
-    view_dirty_ = true;
-    projection_dirty_ = true;
-    return *this;
-  }
+  Camera& SetPerspective(float fov_radian_v, float aspect, float near_clip,
+                         float far_clip);
 
-  Matrix4x4 const& view_matrix() const {
-    if (view_dirty_) {
-      view_matrix_ = CalcViewMatrix();
-      view_dirty_ = false;
-    }
-    return view_matrix_;
-  }
-
-  Matrix4x4 const& projection_matrix() const {
-    if (projection_dirty_) {
-      projection_matrix_ = {};  // TODO: impl projection matrix calc
-      projection_dirty_ = false;
-    }
-    return projection_matrix_;
-  }
-
-  Matrix4x4 const& transform_matrix() const {
-    if (transform_dirty_) {
-      transform_matrix_ = view_matrix() * projection_matrix();
-      transform_dirty_ = false;
-    }
-    return transform_matrix_;
-  }
-
+  Matrix4x4 const& view_matrix() const;
+  Matrix4x4 const& projection_matrix() const;
+  Matrix4x4 const& transform_matrix() const;
   float near_clip() const { return near_clip_; }
-
   float far_clip() const { return far_clip_; }
+  float fov_radian_v() const { return fov_radian_v_; }
+  float fov_radian_h() const { return fov_radian_v_ * aspect_; }
 
  private:
   Vector3 CalcViewDirection() const { return (target_ - pos_).Normalize(); }
-
-  Matrix4x4 CalcViewMatrix() const {
-    Vector3 const n = (target_ - pos_).Normalize();
-    Vector3 const u = (n ^ up_).Normalize();
-    Vector3 const v = u ^ n;
-    auto const posv = pos_ - Point3::Zero();
-    return Matrix4x4({{
-        {u.x(), v.x(), n.x(), 0.0f},
-        {u.y(), v.y(), n.y(), 0.0f},
-        {u.z(), v.z(), n.z(), 0.0f},
-        {-(posv * u), -(posv * v), -(posv * n), 1.0f},
-    }});
-  }
-
-  Matrix4x4 CalcProjectionMatrix() const {
-    Vector3 const n = CalcViewDirection();
-    Vector3 const u = (n ^ up_).Normalize();
-    Vector3 const v = u ^ n;
-    auto const posv = pos_ - Point3::Zero();
-    return Matrix4x4({{
-        {u.x(), v.x(), n.x(), 0.0f},
-        {u.y(), v.y(), n.y(), 0.0f},
-        {u.z(), v.z(), n.z(), 0.0f},
-        {-(posv * u), -(posv * v), -(posv * n), 1.0f},
-    }});
-  }
+  Matrix4x4 CalcViewMatrix() const;
+  Matrix4x4 CalcProjectionMatrix() const;
 
   Point3 pos_{0, 0, 1};
   Point3 target_{0, 0, 0};
   Vector3 up_{0, 1, 0};
-  float fov_radian_{90.0f};
+  float fov_radian_v_{90.0f};
   float aspect_ = 1.0f;
   float near_clip_{0.1f};
   float far_clip_{10000.0f};
