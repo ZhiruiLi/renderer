c47ec20296df783bc8561aedafffa7cc893ed6fe
zhiruili <zr.public@outlook.com>
2021-02-14 23:02:30 +0800

    update
    Former-commit-id: 7cbe1f668974ef46456654873ec951fd400a4d0d [formerly d3cff057c4f2888bc0b27e45985a8de66db9f1b7] [formerly d09b9f6f3b01d77909ba2806997ba19337250305 [formerly a496b1442211a0d09cad100e48291b51c339df78]] [formerly 482c0aca4cc1618d7ceeacff51978d2584160f55 [formerly 18539a32fb40f33818a3dc3cc7f062d9370a6ba7] [formerly b72e106a09698cd8c0ffba3f84b46912cffed3fc [formerly eb34a5cbfff1a2e535bd84eea630afd3e17ca1a2]]] [formerly e5a169eed2242c651ad2c6f727b1aea95cd8684f [formerly b366052033065ba42b3d213b1c18a7652a6f6c07] [formerly 87a93650f8f5eae33cb4a2a8fe59b9f0a7b8e922 [formerly 1e79bb9b91ffe779b1d313a1f08748934d3f94dc]] [formerly 16b77d17256f8ebd358cbc076a6ef77ffa34eb62 [formerly 1ac5774e66e4955c24e23601ff12af850a79805d] [formerly 93f3d5b6ecbd7eae8ca5a544efef97ed19d9a387 [formerly 1121c7874d9217c224fadc0c6c16ee9433f13865]]]] [formerly 4f0c408f379f238277d967205342a6a03010a5de [formerly 6d78e48ec67d3efdf5b2464eff11efbfc7a821dc] [formerly 7e4f5da9195e669f5bbb409582724010d7a6be98 [formerly 30f87bedbfce1a0ad3ef949432a26aba40053216]] [formerly 4c4ea95c85086768b449c439b077f89f7cc564d0 [formerly 63c80938db7a63ec85b4ee7353a52e347ca0024b] [formerly 85f08a137fed9ba758d31f67829da56c8de8bb0c [formerly 5e7d6167baf24282fe5fca7deebacdad45ac4915]]] [formerly f314ac7b4cf6c231c271f02257e6a0e1a9c44332 [formerly 24a6de1b67a4aedefd9dac94c6802510280e4c54] [formerly d76ddd52e85e29970720bdb56fefe5065ff8e2e0 [formerly 9ba76b814a5050e488d2ea2c3c152f96c8f71b02]] [formerly 4380131d1029c0a9ce36b9d046eb85b0794f2d0a [formerly dd0e0f1e439c52dca74ab3ae541ca64dba8491e3] [formerly d9a93abba64812e979dc7ce3d15119a4c9d9c133 [formerly efa0a24200706082a0ba3c55faeb846f6287a284]]]]]
    Former-commit-id: 52e797a73b5e1a6208f68567233b52892468ea30 [formerly 3f62b43f17973a1ff4d7279b2e08778a2506a4d5] [formerly ba8150299ef7b9932cbaa861f73843e80561a8a9 [formerly 56a968d8c6400a160bc0ccc659bfb7856cb3bd8b]] [formerly 0ee114aa9a244c19c0dd9a336c9d4671b648a643 [formerly 031e03516b0583c33cd23f683736f196ea627f34] [formerly 333edd5e032d701ba6e9acb97ec398994449f0bf [formerly 03e5aa37dc149b2632729dc34ca50a0710ad4b5e]]] [formerly 2045c11c7fc452c4ad38d7dd53996284867fc481 [formerly abc71d60852c825d0bcbc215119b43a6151f91e7] [formerly 1b802099b4d007b9ba0e571a365ec3fc425af6cd [formerly e9aa9b73a3b9885344a7dc037056ae2bd8ae21a0]] [formerly 5e8b984355cce7635e9ad37cf2bfd831d24a9e3b [formerly f7ac744aa91c4b1b8f189bc237ac88500b519777] [formerly 6658dbc5857f7105f7362644f9d09edd6038731f [formerly 430a9f1dbd9b52970952576b1990781f206eaf28]]]]
    Former-commit-id: e9028273f78fe5d7c93ba280abaef28183fa74b6 [formerly ad25076e2055e5a2a497d21a4033932a77b0ddda] [formerly e0869af7ee015652caf4e3ba5c5d1518d0cb4d82 [formerly 145bf8795a7f9670150b08d22a8a928c1e4a7cf4]] [formerly e0e6360df5594e715b29c3c09fb22ef5b3591a13 [formerly 6c6fcfde841a4b3ed224326ff30e9a399cd77386] [formerly 47ff9a8bb665b4236192cf327625d360dddc3b73 [formerly 576bcf794c3e19e31b04967a5f3714a82a2f1c23]]]
    Former-commit-id: a6c6ff6198978fe120b3f10d71f43ea1741bec55 [formerly ada04b96c51d2b3ea931c8f9e80dc5814e60c71e] [formerly a3e9c416e5e4498f68fc98ac5275386d99735fdd [formerly 8b161be456746d25fd4105b61f54afe2c7fe14e4]]
    Former-commit-id: a47ee6d46d8f5e94f3a6a4df8d1659633dba7cf1 [formerly 34f8b7b0965ae20c3045385fabf52a4a1d02977f]
    Former-commit-id: d3523156ba5870c8def22f1b0dece55236ebe2a9

--- tests/trapezoid_test.cc
+++ tests/trapezoid_test.cc
@@ -50,3 +50,39 @@ TEST(DrawTest, CutTriangle_SplitTriangleIntoTrapezoids_2Right) {
   ASSERT_EQ(traps[1].right.top.pos(), p1);
   ASSERT_EQ(traps[1].right.bottom.pos(), p0);
 }
+
+TEST(DrawTest, CutTriangle_SplitTriangleIntoTrapezoids_1FlatTop) {
+  auto const p0 = Vector4(1, 0, 0, 0);
+  auto const p1 = Vector4(3, 2, 0, 0);
+  auto const p2 = Vector4(-5, 2, 0, 0);
+  std::array<Vertex, 3> verts = {
+      Vertex(p2, {}, {}, {}),
+      Vertex(p0, {}, {}, {}),
+      Vertex(p1, {}, {}, {}),
+  };
+  std::array<Trapezoid, 2> traps{};
+  auto const count = trapezoids::CutTriangle(verts, &traps);
+  ASSERT_EQ(count, 1);
+  ASSERT_EQ(traps[0].left.top.pos(), p2);
+  ASSERT_EQ(traps[0].left.bottom.pos(), p0);
+  ASSERT_EQ(traps[0].right.top.pos(), p1);
+  ASSERT_EQ(traps[0].right.bottom.pos(), p0);
+}
+
+TEST(DrawTest, CutTriangle_SplitTriangleIntoTrapezoids_1FlatBottom) {
+  auto const p0 = Vector4(8, 1, 0, 0);
+  auto const p1 = Vector4(-1, 1, 0, 0);
+  auto const p2 = Vector4(1, 5, 0, 0);
+  std::array<Vertex, 3> verts = {
+      Vertex(p2, {}, {}, {}),
+      Vertex(p0, {}, {}, {}),
+      Vertex(p1, {}, {}, {}),
+  };
+  std::array<Trapezoid, 2> traps{};
+  auto const count = trapezoids::CutTriangle(verts, &traps);
+  ASSERT_EQ(count, 1);
+  ASSERT_EQ(traps[0].left.top.pos(), p2);
+  ASSERT_EQ(traps[0].left.bottom.pos(), p1);
+  ASSERT_EQ(traps[0].right.top.pos(), p2);
+  ASSERT_EQ(traps[0].right.bottom.pos(), p0);
+}
