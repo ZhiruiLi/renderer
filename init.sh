9b997654f251647e567373ac5331e2eddda88829
zhiruili <zr.public@outlook.com>
2021-02-20 15:01:41 +0800

    init script
    Former-commit-id: f2992d4a0cf76eb69997fc8488087f55434e066e [formerly 478021fcd1fbff8c8b4c50b4d525515d73e672fd] [formerly 16dc0017f264eefa4721d19e9c856e80d2753057 [formerly e97d33d4b7671690e67e2a6b7d28494d8ddd6074]] [formerly af828b2b5481110ad5e61f88343fc08880b6b2b6 [formerly 13efdef8543e7b1bb8985dc6ad7314b0a6a59c33] [formerly 0ecd41989731b17f55b3046c39458848eae586c8 [formerly 48feb19cd4249c46545bed069ec88a1ddfcdff4d]]] [formerly 8816dfb8776d40fbc521a0845fa23d3cf2b345bb [formerly 0b4e673f9ba0a6f08407cc87d54b1c2f48f32dcf] [formerly 3d61d33f284908a1c8c4eb196805713e4f3e6ec4 [formerly c5e484af959d1d5e1d8c7db47a2a72656b5ee6a8]] [formerly 8748e2b5c98b9d1f115549bf09b8fa8c48294e19 [formerly ec2f6dc5d5e85544a3630aa2e0a7ad2fa400b8d4] [formerly 66238e51443ece1959403a2b096e1427ab882dc9 [formerly 70e89546372576f43494c290a42523bf949a5035]]]] [formerly c36041adbfb6f9f2e9695997bfae8b0a8d76139f [formerly f9399881617476119cf0d22a2b2190baadd3ead0] [formerly 6a3d1dd222812dd771beb8eff182e5f263d8be72 [formerly b05c0476ae52542c83030945937935c378cb73c8]] [formerly f3ffd528eacce07488d20036c4638ebb13d75508 [formerly 502ac6a79c675e8294e4f1a04ec87a4eff34aa58] [formerly eaf06624080a3c364b6a7d60ea491b2d1b53c933 [formerly dce3c0f39c99009f42e74a666387c85788a59b38]]] [formerly b7a5762576b34f787dbcac2eee2d30e1b0d4f44f [formerly 7fe8637cb05ca86929eb45cc84b6f7a18c91662d] [formerly 4e84a14036fa26e762b7638b1a7518af4d498320 [formerly e0ca5f68ec0b5e5b8df473e1a6fddf9d4f106551]] [formerly 567bbbce3162c9223a340de8354875fd98b7155b [formerly a3cb93eeace67c8eea2c7df2fa9dc5fbe4ccb961] [formerly 50d6a27983c95f1a1f28c052512f05be82d7dc4b [formerly 07e1b2da5df1abe4c328b0ef6587406deb4cf05a]]]]]
    Former-commit-id: dbdf3bdab4a71475225abcd046966eb25a5dff51 [formerly e625523a5a607faccda5283a71bf12b9884a3444] [formerly f002e7ebf7bbde1bb009f95d4d8e70d43578aa91 [formerly 747d73472a9d21a2b9d5e2cd2412287447f4caaf]] [formerly da3c43e4554ef98b9dd6b4348573835e0264ad30 [formerly 9847437d2531d70ba8e8d96eff36a384b504f2be] [formerly bfbf84f989f51cb8b40d3aabbf297ff38153a93d [formerly 0c0de48611606912fa13b5844dfb516e10910adf]]] [formerly f050538db8817e12f3e85234939c5fc70469e18c [formerly 521bce0c1bc45335247d8e25c17e4f6cfd382d79] [formerly 5270eaccb203360aadf7908859e094fb91920f83 [formerly e66992001aa5c2dd4ad99e6f495eb63826b6563a]] [formerly 41738c8e3949a9b5d75dc027cb88154f13f98cb8 [formerly bfb1fc826323604dcea494f8a33baea1647148ca] [formerly 92dcdaf10de149bf155eabfa3f1d357d7364978c [formerly d2b3d762484dbb8d9f9f212a432f44a22ee6b222]]]]
    Former-commit-id: 0f3db5f87ed5a0ba96cfbd9cea67c4b46c4d7317 [formerly 75ab1700de3bf82e1c54a2326d0fea44fa4176fc] [formerly e7960dcb22bee2043e36e7ebf4c6dac7ed67749b [formerly 854b934dc63fe5b3a6e6c9ccf1b00e26c9c828d5]] [formerly d3989506e4638f9b791fadd61a449b963a0bbb41 [formerly d00414882bc159084a86a4696ae03f37957ea949] [formerly 9d11621a12a3471ec96e2cd4dffca6a171a36a2e [formerly 5b16d3d475a688f70b33d065825c9eb1912c73b7]]]
    Former-commit-id: 7ff67027118f4188f38bfd184a93c6b6faa13acf [formerly c782c00b6edd1fa5614a73dd49fbf187eb70784c] [formerly 7a84974401f4cf444ec51e8246eb598538f42088 [formerly 2e517fcfc276e8ff6e453c7e80bb343ca3518b7a]]
    Former-commit-id: 29c99ad8fa7b2aa5a602e1eda201e4ec50210db9 [formerly 21f92233fc511012cf12c22bfeba7f2fbaa35e1a]
    Former-commit-id: 4898277d8d5e5593bbe4f4ecda848ff3a7e47a84

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# let script exit if a command fails
+set -o errexit
+# let script exit if an unsed variable is used
+set -o nounset
+# catch mysqldump fails in e.g. mysqldump | gzip.
+# The exit status of the last command that threw a non-zero exit code is returned.
+set -o pipefail
+# trace what gets executed. Useful for debugging.
+# set -o xtrace
+
+# Set magic variables for current file, basename, and directory at the top of your script for convenience.
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+# if [[ $# -ne 1 ]] ; then
+#   echo "usage:" $0 "<ARG>"
+#   exit 1
+# fi
+
+arg1="${1:-}"
+
+mkdir build && cd build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+
